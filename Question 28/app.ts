06c0383e9b06bee5c202821f8cc3895b8bd17c74
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-05-14 11:46:18 +0500

    adding 10 day of challenge

---
+++ Question 28/app.ts
@@ -0,0 +1,11 @@
+// *Question 28:* Stages of Life: Determine a person’s life stage with an if-else chain.
+
+// *Explain & TIP:* Use age to check the life stage. This practice shows how to handle multiple conditions with an if-else chain.
+let age=25;
+
+if (age < 2){console.log('The person is baby')}
+if (age < 4){console.log('The person is toddler')}
+if (age < 13){console.log('The person is kid')}
+if (age < 20){console.log('The person is teenager')}
+if (age < 65){console.log('The person is adult')}
+else{console.log('The person is elder')}
---
+++ Question 29/app.ts
@@ -0,0 +1,10 @@
+
+// *Question 29:* Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.
+
+// *Explain & TIP:* Arrays can store multiple items like fruit names. Use if statements to check for specific fruits.
+
+let favorite_fruit=["watermelon","banana","grapes"]
+
+if (favorite_fruit.includes ("watermelon")){console.log('I like watermelon')}
+ if (favorite_fruit.includes ("grapes")){console.log('I like grapes')}
+ if (favorite_fruit.includes ("banana")){console.log('I like banana')}
